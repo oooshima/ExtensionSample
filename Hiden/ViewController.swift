98ee1674366c6566c31a7b4009a4de8f12f52d50
oshimaharuna <ooooooshim115@gmail.com>
2019-06-16 00:27:07 +0900

    clean ViewController

--- Hiden/ViewController.swift
+++ Hiden/ViewController.swift
@@ -10,36 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var alertButton: UIButton!
-    @IBOutlet weak var alertButton2: UIButton!
-    @IBOutlet weak var alertButton3: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func showAlert(_ sender: Any) {
-        UIAlertController.show(title: "alart1", message: "OKとキャンセルだよ", okAction: printOK, cancelAction: nil)
-    }
-    
-    @IBAction func showAlert2(_ sender: Any) {
-        UIAlertController.show(title: "alart2", message: "OKだけだよ", okAction: nil)
-    }
-    
-    @IBAction func showAlert3(_ sender: Any) {
-        UIAlertController.show(title: "ActionSeet", message: "選択肢は増やせるよ", cancelAction: printCancel, defaultButton: ["Japan","Chaina","USA"], defaultAction: printName(name:))
-    }
-    
-    func printOK(){
-        print("ok")
-    }
-    
-    func printCancel(){
-        print("cancel")
-    }
-    
-    func printName(name:String){
-        print(name)
     }
     
 }
